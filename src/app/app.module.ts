2ef826ac98f68d6a781a6a971c550df01d1eef37
ImsirovicAjdin <ajdika@live.com>
2019-08-29 08:46:29 +0200

    Our Angular8 app with components plugged in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CarouselsComponent } from './carousels/carousels.component';
+import { DatepickersComponent } from './datepickers/datepickers.component';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ 
+    AppComponent, HelloComponent, 
+    ButtonsComponent, CarouselsComponent, DatepickersComponent, DropdownsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/dropdowns/dropdowns.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-dropdowns',
+  templateUrl: './dropdowns.component.html'
+})
+export class DropdownsComponent {}
