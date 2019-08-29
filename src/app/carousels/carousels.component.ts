ecc7dab55c9a5377105c9a640b5d37c0210f207d
ImsirovicAjdin <ajdika@live.com>
2019-08-29 17:09:03 +0200

    Added a dynamic ngx-bootsrtrap carousel

--- src/app/carousels/carousels.component.ts
+++ src/app/carousels/carousels.component.ts
@@ -4,4 +4,24 @@ import { Component } from '@angular/core';
   selector: 'my-carousels',
   templateUrl: './carousels.component.html'
 })
-export class CarouselsComponent { }
+export class CarouselsComponent {
+  slides: { image: string }[] = [];
+  activeSlideIndex = 0;
+ 
+  constructor() {
+    for (let i = 0; i < 4; i++) {
+      this.addSlide();
+    }
+  }
+ 
+  addSlide(): void {
+    this.slides.push({
+      image: `assets/images/nature/${this.slides.length % 8 + 1}.jpg`
+    });
+  }
+ 
+  removeSlide(index?: number): void {
+    const toRemove = index ? index : this.activeSlideIndex;
+    this.slides.splice(toRemove, 1);
+  }
+}
